890575b6543edb708b3ee9d31af0295ac84914b7
bvictora <vrr46@hotmail.com>
2025-01-29 17:20:23 +0100

    purchases coding

--- notebooks/dictionaries.py
+++ notebooks/dictionaries.py
@@ -5,3 +5,10 @@ regions = {
     'Eastern Europe': ['Bulgaria', 'Croatia', 'Estonia', 'Latvia', 'Lithuania', 'Romania', 'Serbia', 'North Macedonia', 'Montenegro', 'Bosnia and Herzegovina', 'Albania', 'Kosovo*'],
     'Southern Europe': ['Greece', 'Italy', 'Malta', 'Portugal', 'Spain', 'Cyprus', 'Türkiye']
 }
+
+
+education = {
+    'Low': ['Individuals aged 16-24 with low education', 'Individuals aged 25 to 54 with low formal education', 'Individuals aged 55 to 74 with low formal education'],
+    'Medium': ['Individuals aged 16-24 with medium formal education', 'Individuals aged 25 to 54 with medium formal education', 'Individuals aged 55 to 74 with medium formal education'],
+    'High': ['Individuals aged 16-24 with high formal education', 'Individuals aged 25 to 54 with high formal education', 'Individuals aged 55 to 74 with high formal education'],
+}
